8ef22d03326caf0dc2ea3a30549630757493a5e2
xpizza <raining0807@163.com>
2018-01-16 17:04:04 +0800

    Sqllite

--- src/main/java/com/xpizza/classical/Test.java
+++ src/main/java/com/xpizza/classical/Test.java
@@ -4,7 +4,7 @@ public class Test {
 
     public static void main(String[] args){
         Pool pool = new Pool("sqllite",5,"jdbc:sqlite:test.db", null,null, "org.sqlite.JDBC");
-        
+
     }
 
 }
